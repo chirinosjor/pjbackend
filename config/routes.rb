fe4a22a7dea80ea3d186a7a3b36472ce5b2f29f9
Jorge Chirinos <chirinosjor@gmail.com>
2021-02-05 18:43:53 -0300

    Added root

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/health', to: 'health#health'
 
+  root "orders#index"
+
   resources :stores
   resources :products
   resources :orders
